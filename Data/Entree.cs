0c80ca3afdbb5573c6e9112ef2f8baf94e3697ab
John Boehm <jhboehm@users.campus>
2020-02-03 16:51:15 -0600

    added chilicheesefries

---
+++ Data/Entree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public abstract class Entree
+    {
+        /// <summary>
+        /// Gets the price of the entree
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// Gets the calories of the entree
+        /// </summary>
+        public abstract uint Calories { get; }
+        /// <summary>
+        /// Gets any special instructions
+        /// </summary>
+        public virtual List<string> SpecialInstructions { get; }
+    }
+}
